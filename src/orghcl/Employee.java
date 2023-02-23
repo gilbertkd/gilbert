77c5a1f5ef1f72a02a584a29f17075264b6eecc1
gilbertkd <gilbertkd8@gmail.com>
2023-02-23 12:21:22 +0530

    new

---
+++ src/orghcl/Employee.java
@@ -0,0 +1,5 @@
+package orghcl;
+
+public class Employee {
+
+}
---
+++ src/orghcl/clientdetails.java
@@ -0,0 +1,34 @@
+package orghcl;
+
+import java.net.PasswordAuthentication;
+
+public class clientdetails extends Employee {
+	private void clientname() {
+	System.out.println("enter the client name");
+	}
+	private void clientid() {
+	System.out.println("enter the client id");
+	}
+	private void username() {
+		System.out.println("enter the username");
+	}
+	private void password() {
+		System.out.println("enter the password");
+	}
+	public static void main(String[] args) {
+    clientdetails a = new clientdetails();
+	a.clientname();
+	a.clientid();
+	a.username();
+	a.password();
+	}
+	
+    
+    
+    
+    
+    
+    
+	}
+
+
